38e4672bfd015106ee054cb32c86970ee3246ecf
taiphlosion <ttai8501@gmail.com>
2025-02-04 00:20:54 -0500

    Set up basic frontend and backend

---
+++ ChompIn/server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CIS4914---Senior-Project
+
+## Basic Start
+To start the basic front-end, go to **client** and directory and run 
+`npx expo start`
+
+For backend, it's really basic and simple made with Node and Express
